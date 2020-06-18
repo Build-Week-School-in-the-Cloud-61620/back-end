3ceb75dc057c79972bdfe4ef6ea7dc9061749005
SMoore3773 <Smooresculpture@gmail.com>
2020-06-18 00:11:34 -0400

    test folder added, auth middleware in, routers in file

---
+++ auth/auth-router.js
@@ -0,0 +1,23 @@
+//imports
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = require('../../config/secrets');
+
+const {isValid} = require('./valid-check');
+
+//token generation on login
+function generateToken(user){
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        role: user.role
+    };
+    const options = {
+        expiresIn:'2h'
+    };
+    return jwt.sign(payload, secret.jwtSecret, options)
+}
+
+
+module.exports = router;
