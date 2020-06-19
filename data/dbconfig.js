a37091a5bab063c4627720718a0b3c3f8627b91c
Wais Almakaleh <you@example.com>
2020-06-18 21:53:38 -0400

    i think i added postgres

--- data/dbconfig.js
+++ data/dbconfig.js
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-const knex = require("knex");
 
-const knexConfig = require("../knexfile.js");
-module.exports = knex(knexConfig.development);
-=======
-<<<<<<< HEAD
 const knex = require("knex");
 
 const knexfile = require("../knexfile.js");
 const environment = process.env.NODE_ENV || "development";
 
 module.exports = knex(knexfile[environment]);
-=======
-const knex = require('knex');
 
-const knexConfig  = require('../knexfile.js')
-module.exports = knex(knexConfig.development)
->>>>>>> a5ae9dc2ec16f7be8f52a98cf776907631005555
->>>>>>> d6b079389219c1fca4a76c1e0904e8a6fca36d5f
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+const pgConnection = process.env.DATABASE_URL || 'postgresql://postgress@localhost/login'
 
 require("dotenv").config();
 
