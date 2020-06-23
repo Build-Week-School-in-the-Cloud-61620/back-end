4f01c53683e69f951f3fb37dc1754796cfaa68ea
Wais Almakaleh <you@example.com>
2020-06-22 23:09:23 -0400

    incase I forgot to push the latest update

--- api/routers/student-router.js
+++ api/routers/student-router.js
@@ -1,27 +1,6 @@
 const router = require('express').Router()
 const db = require('./router-models')
 
-router.get('/', (req, res) => {
-  db.getStudent()
-    .then((get) => {
-      res.status(200).json(get)
-    })
-    .catch((err) => {
-      res.status(500).json(err)
-    })
-})
-
-router.post('/', (req, res) => {
-  const { body } = req
-  db.addStudent(body)
-    .then((post) => {
-      res.status(201).json(post)
-    })
-    .catch((err) => {
-      res.status(500).json(err.message)
-    })
-})
-
 router.get('/:id', (req, res) => {
   const { id } = req.params
   db.getStudentById(id)
