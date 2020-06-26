ca2399930ffb0ce0fc7c7e84ca51eaec4bd3743b
SMoore3773 <Smooresculpture@gmail.com>
2020-06-25 22:53:56 -0400

    router tests done

--- __tests__/admin-router.spec.js
+++ __tests__/admin-router.spec.js
@@ -11,11 +11,10 @@ describe('testing env', () => {
 
 describe('admin router', () => {
   beforeEach(async () => { await db('admin').truncate() })
-
   const admin = { username: 'admin', name: 'admin', password: 'password', role: 'admin', email: 'admin@email.com' }
   const adminCreds = { username: 'admin', password: 'password', role: 'admin' }
 
-  it('sould return 200 on successful get', async () => {
+  it('sould return 200 on successful GET', async () => {
     await request(server).post('/api/auth/register').send(admin)
     const loginRes = await request(server).post('/api/auth/login').send(adminCreds)
     const res = await request(server).get('/api/admin').set('authorization', loginRes.body.token)
@@ -29,32 +28,52 @@ describe('admin router', () => {
     expect(res.status).toBe(200)
   })
 
-  it('should get all students with status 200', async () => {
+  it('should GET all students with status 200', async () => {
     await request(server).post('/api/auth/register').send(admin)
     const loginRes = await request(server).post('/api/auth/login').send(adminCreds)
     const res = await request(server).get('/api/admin/students/all').set('authorization', loginRes.body.token)
     expect(res.status).toBe(200)
   })
-  it('should get all volunteers with status 200', async () => {
+
+  it('should GET all volunteers with status 200', async () => {
     await request(server).post('/api/auth/register').send(admin)
     const loginRes = await request(server).post('/api/auth/login').send(adminCreds)
     const res = await request(server).get('/api/admin/volunteer/all').set('authorization', loginRes.body.token)
     expect(res.status).toBe(200)
   })
-  it('should get volunteer by id with status 201', async () => {
+
+  it('should GET volunteer by id with status 201', async () => {
     await request(server).post('/api/auth/register').send(admin)
     const loginRes = await request(server).post('/api/auth/login').send(adminCreds)
     const res = await request(server).get('/api/admin/volunteer/1').set('authorization', loginRes.body.token)
     expect(res.status).toBe(201)
   })
-  // not working***** maybe issue with tables
-  it('should get tasks by admin id with status 201', async () => {
-    const admin = { username: 'admin', name: 'admin', password: 'password', role: 'admin', email: 'admin@email.com' }
-    const adminCreds = { username: 'admin', password: 'password', role: 'admin' }
+
+  it('should GET tasks by admin id with status 201', async () => {
     await request(server).post('/api/auth/register').send(admin)
     const loginRes = await request(server).post('/api/auth/login').send(adminCreds)
     const res = await request(server).get('/api/admin/1/tasks').set('authorization', loginRes.body.token)
     expect(res.status).toBe(201)
   })
-  
+
+  it('should POST task by admin and volunteer id', async () => {
+    await request(server).post('/api/auth/register').send(admin)
+    const loginRes = await request(server).post('/api/auth/login').send(adminCreds)
+    const res = await request(server).post('/api/admin/1/tasks/1').set('authorization', loginRes.body.token).send({ description: 'new task for testing', completed: false })
+    expect(res.status).toBe(200)
+  })
+
+  it('should PUT to edit a task and respond with 200', async () => {
+    await request(server).post('/api/auth/register').send(admin)
+    const loginRes = await request(server).post('/api/auth/login').send(adminCreds)
+    const res = await request(server).put('/api/admin/1/tasks').set('authorization', loginRes.body.token).send({ completed: true })
+    expect(res.status).toBe(200)
+  })
+
+  it('should DELTE task successfully and respond with 200', async () => {
+    await request(server).post('/api/auth/register').send(admin)
+    const loginRes = await request(server).post('/api/auth/login').send(adminCreds)
+    const res = await request(server).delete('/api/admin/1/tasks').set('authorization', loginRes.body.token)
+    expect(res.status).toBe(200)
+  })
 })
--- __tests__/volunteer-router.spec.js
+++ __tests__/volunteer-router.spec.js
@@ -14,12 +14,14 @@ describe('volunteer-router', () => {
     const res = await request(server).get('/api/volunteer/all').set('authorization', loginRes.body.token)
     expect(res.status).toBe(200)
   })
+
   it('return status 200 on get by id', async () => {
     await request(server).post('/api/auth/register').send(newVolunteer)
     const loginRes = await request(server).post('/api/auth/login').send(newVolunteerCreds)
     const res = await request(server).get('/api/volunteer/1').set('authorization', loginRes.body.token)
     expect(res.status).toBe(200)
   })
+
   it('returns status 200 on get tasks by id', async () => {
     await request(server).post('/api/auth/register').send(newVolunteer)
     const loginRes = await request(server).post('/api/auth/login').send(newVolunteerCreds)
@@ -34,6 +36,7 @@ describe('volunteer-router', () => {
     console.log(res.status)
     expect(res.status).toBe(201)
   })
+
   it('deletes volunteer task successfully', async () => {
     await request(server).post('/api/auth/register').send(newVolunteer)
     const loginRes = await request(server).post('/api/auth/login').send(newVolunteerCreds)
